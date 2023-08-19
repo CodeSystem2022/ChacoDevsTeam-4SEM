71bf9012d13e891a81c6d232a2b2a2cf2ad6be8e
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2023-08-18 21:21:59 -0300

    Java- Método eliminarEstudiante (#16)

---
+++ JAVA/SistemaEstudiantes/src/main/java/UTN/Main.java
@@ -0,0 +1,12 @@
+package UTN;
+
+import UTN.conexion.Conexion;
+public class Main {
+    public static void main(String[] args) {
+        var conexion = Conexion.getConnection();
+        if(conexion != null)
+            System.out.println("Conexion exitosa: "+conexion);
+            else
+            System.out.println("Error al conectarse");
+    }//Fin main
+}//Fin clase
