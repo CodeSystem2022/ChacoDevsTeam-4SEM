4fe2e189c38fdea10f5be95dc9da691eb21f178f
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-08-19 16:15:26 -0300

    Ticket#7 y Ticket#8 PYTHON (#24)
    Se hizo el test de clase usuarioDAO con funcionamiento optimo y se creó la clase menu_app_usuario con su respectivo ciclo while.

---
+++ PYTHON/Leccion07/capa_datos_persona/menu_app_usuario.py
@@ -0,0 +1,9 @@
+opcion = None
+while opcion != 5:
+    print("Opciones: ")
+    print("1). Listar Usuarios")
+    print("2). Agregar Usuario")
+    print("3). Modificar Usuario")
+    print("4). Eliminar Usuario")
+    print("5). Salir")
+    opcion = int(input("Digite la opcion (1-5): "))
