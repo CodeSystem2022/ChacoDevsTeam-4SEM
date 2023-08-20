772a634bd53fcd056fc373105681c1c3050d6624
Alejo Gutierrez <alejohgutierrez@hotmail.com>
2023-08-20 14:57:56 -0300

    Python- Clase usuarioDAO#6

---
+++ PYTHON/Leccion07/capa_datos_persona/usuario_dao.py
@@ -0,0 +1,51 @@
+from PYTHON.Leccion07.capa_datos_persona.cursor_del_pool import CursorDelPool
+from PYTHON.Leccion07.capa_datos_persona.usuario import Usuario
+from logger_base import log
+
+class UsuarioDao:
+    '''
+    DAO -> Data Access Object Para la tabla de usuario
+    CRUD -> Create - Read - Update - Delete
+    '''
+
+    _SELECT = 'SELECT * FROM usuario ORDER BY id_usuario'
+    _INSERTAR = 'INSERT INTO usuario(username, password) VALUE (%s, %s)'
+    _ACTUALIZAR = 'UPDATE usuario SET username=%s, password=%s WHERE id_usuario=%s'
+    _ELIMINAR = 'DELETE FROM usuario WHERE id_usuario=%s'
+
+    @classmethod
+    def seleccionar(cls):
+        with CursorDelPool() as cursor:
+            log.debug('Seleccionando usuarios')
+            cursor.execute(cls._SELECT)
+            registros = cursor.fetchall()
+            usuarios = []
+            for registro in registros:
+                usuario = Usuario(registro[0], registro[1], registro[2])
+                usuarios.append(usuario)
+            return usuarios
+
+    @classmethod
+    def insertar(cls, usuario):
+        with CursorDelPool() as cursor:
+            log.debug(f'Usuario a insertar: {usuario}')
+            valores = (usuario.username, usuario.password)
+            cursor.execute(cls._INSERTAR, valores)
+            return cursor.rowcount
+
+    @classmethod
+    def actualizar(cls, usuario):
+        with CursorDelPool() as cursor:
+            log.debug(f'Usuario a actualizar: {usuario}')
+            valores = (usuario.username, usuario.password, usuario.id_usuario)
+            cursor.execute(cls._ACTUALIZAR, valores)
+            return cursor.rowcount
+
+    @classmethod
+    def eliminar(cls, usuario):
+        with CursorDelPool() as cursor:
+            log.debug(f'Usuario a eliminar: {usuario}')
+            valores = (usuario.id_usuario,)
+            cursor.execute(cls._ELIMINAR, valores)
+            return cursor.rowcount
+
