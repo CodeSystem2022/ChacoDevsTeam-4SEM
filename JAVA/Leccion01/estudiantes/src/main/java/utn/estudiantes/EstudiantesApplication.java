65f57710a5059f2e1ce3075f4e420c87ad9a3291
Agustin Gutierrez <yo_agustingutierrez@hotmail.com>
2023-08-25 01:24:24 -0300

    JAVA-SPRINGBOOT-Creación proyecto estudiante#36

---
+++ JAVA/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -0,0 +1,13 @@
+package utn.estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesApplication.class, args);
+	}
+
+}
