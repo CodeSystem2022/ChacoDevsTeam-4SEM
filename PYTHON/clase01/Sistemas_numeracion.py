5b8caa0554753c0bc5b95b1fbfdadeb7f2ea6941
Tatiana Tamara, Giménez Ríos <103543709+TatianaGimenezRios@users.noreply.github.com>
2023-08-29 23:02:56 -0300

    PYTHON - Sistemas de Numeración #43 (#48)

---
+++ PYTHON/clase01/Sistemas_numeracion.py
@@ -0,0 +1,37 @@
+# Profundizando en los sistemas de numeración
+
+# Sistema decimal
+a = 10
+print(f"a decimal: {a}")
+
+# Sistema binario
+a = 0b1010
+print(f"a binario: {a}")
+
+# Sistema octal
+a = 0o12
+print(f"a octal: {a}")
+
+# Sistema hexadecimal
+a = 0xA
+print(f"a hexadecimal: {a}")
+
+# Base Decimal
+a = int('23',10 )
+print(f"a = base decimal: {a}")
+
+# Base Binario
+a = int('10111', 2)
+print(f"a = base binario: {a}")
+
+# Base Octal
+a = int('27', 8)
+print(f"a = base octal: {a}")
+
+# Base Hexadecimal
+a = int('17', 16)
+print(f"a = base hexadecimal: {a}")
+
+# Base 5 sus valores son de 0 a 4
+a = int('344', 5)
+print(f'a = Base 5: {a}')
