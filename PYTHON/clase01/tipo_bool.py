a0809a65ff3f3b49e026b8844969063d7161a3b1
Renzo Espinola <70241433+Renzo-Espinola@users.noreply.github.com>
2023-09-06 18:34:54 -0300

    PYTHON-Strings y Colecciones (#73)

--- PYTHON/clase01/tipo_bool.py
+++ PYTHON/clase01/tipo_bool.py
@@ -8,3 +8,47 @@ print(f'valor: {valor}, Resultado: {resultado}')
 valor = 0.1
 resultado = bool(valor)
 print(f'valor: {valor}, Resultado: {resultado}')
+
+# Tipo string -> False '', True para los demas valores
+valor = ''
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 'Hola'
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+# Tipo colecciones -> False para colecciones vacias
+# Tipo colecciones -> True para todas las demas
+# Lista
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacia: {valor}, Resultado: {resultado}')
+
+valor = [2, 3, 4]
+resultado = bool(valor)
+print(f'valor de una lista con elementos: {valor}, Resultado: {resultado}')
+
+# Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacia: {valor}, Resultado: {resultado}')
+
+valor = (5,)
+resultado = bool(valor)
+print(f'valor de una tupla completa: {valor}, Resultado: {resultado}')
+
+# Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacio: {valor}, Resultado: {resultado}')
+
+valor = {'Nombre': 'Juan','Apellido': 'Perez'}
+resultado = bool(valor)
+print(f'valor de un diccionario completo: {valor}, Resultado: {resultado}')
+
+
+
+
+
+
