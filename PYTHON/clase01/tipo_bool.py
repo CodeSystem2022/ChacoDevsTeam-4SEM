6ac2be16384e9b85892dd943697edd582504b7b8
Tatiana Tamara, Giménez Ríos <103543709+TatianaGimenezRios@users.noreply.github.com>
2023-09-06 14:58:24 -0300

    PYTHON-NaN Y Bool (#72)

---
+++ PYTHON/clase01/tipo_bool.py
@@ -0,0 +1,10 @@
+
+# Bool contiene los valores de True y False
+# Los tipos numéricos, es false para el 0 (cero), true para los demás valores
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
