72281fc83aabc6e033dfd142c521ba8746acab24
AlejoHGZ <106632526+AlejoHGZ@users.noreply.github.com>
2023-09-08 20:57:27 -0300

    JAVA-Nuevos Metedos 2da parte#75 (#91)

--- JAVA/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ JAVA/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,18 @@
 package utn.estudiantes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
