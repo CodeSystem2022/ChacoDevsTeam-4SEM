c23d548a35ff14edb3a924b4d8138c4154b269a8
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-09-16 16:26:42 -0300

    JS-displayCartCounter#100 (#117)
    Se creó y se modificó las respectivas lineas de codigo.

--- JAVASCRIPT/e-commerce2022/client/js/index.js
+++ JAVASCRIPT/e-commerce2022/client/js/index.js
@@ -18,19 +18,22 @@ productos.forEach((product) => {
         const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
         
         if(repeat){
-          cart.map((prod)=> {
-            if(prod.id === product.id){
-                prod.quanty++;
-            }
-          });  
-        }else {
-         cart.push({
-                id: product.id,
-                productName: product.productName,
-                price: product.price,
-                quanty: product.quanty,
-                img: product.img,  
-            });   
+          cart.map((prod) => {
+              if(prod.id === product.id){
+                  prod.quanty++;
+                  displayCartCounter();
+              }
+          });
+      } else {
+        cart.push({
+            id: product.id,
+            productName: product.productName,
+            price: product.price,
+            quanty: product.quanty,
+            img: product.img,  
+            });
+            displayCartCounter();
         }
     });
 });
+
