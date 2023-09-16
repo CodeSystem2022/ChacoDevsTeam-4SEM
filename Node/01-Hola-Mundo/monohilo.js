858284e0f7abecbf71546efca4d9d9e2c2e611ff
Tatiana Tamara Gimenez Rios <tatagimeri07@gmail.com>
2023-09-16 16:14:03 -0300

    NODE-SetInterval#99

--- Node/01-Hola-Mundo/monohilo.js
+++ Node/01-Hola-Mundo/monohilo.js
@@ -1,1 +1,14 @@
+
 console.log('Hola a todo el grupo de Chaco Devs Team')
+
+var i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+
+    //if(i === 5){
+    //    console.log('Forzamos un error');
+    //    var a = 3 + z;
+    //}
+}, 1000);
+console.log('Segunda instrucción');
