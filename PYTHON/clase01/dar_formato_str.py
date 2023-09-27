87dd946e8df07901bce134c0447aba515ba0072c
Juan Valladares <juan_1@outlook.es>
2023-09-27 15:23:18 -0300

    Test ticket Python #159
    VIDEO 3 = (Se requiere crear el metodo format en "dar_formato_str")

--- PYTHON/clase01/dar_formato_str.py
+++ PYTHON/clase01/dar_formato_str.py
@@ -4,6 +4,16 @@ nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
 
+#Creamos una tupla
 persona = ('Carla', 'Gomez', 5000.00)
 mensaje_con_formato = 'Hola %s %S . Tu sueldo es %.2f' # % persona Aqui le pasamos el objeto que es tupla.
-print(mensaje_con_formato % persona)
+# print(mensaje_con_formato % persona)
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+#mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje_con_formato
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
