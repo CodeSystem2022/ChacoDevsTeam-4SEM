61a06517cb5be8f543caeab24b9142e25e29e09a
AlejoHGZ <106632526+AlejoHGZ@users.noreply.github.com>
2023-10-01 21:59:32 -0300

    JAVA-Capa de Servicio-Parte 2 -> Clase LibroServicio => implementación#181 (#191)

--- JAVA/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/servicio/LibroServicio.java
+++ JAVA/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/servicio/LibroServicio.java
@@ -1,4 +1,32 @@
 package utn.tienda_libros.servicio;
 
-public class LibroServicio {
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class LibroServicio implements ILibroServicio {
+
+    @Autowired
+    private LibroRepositorio libroRepositorio;
+
+    @Override
+    public List<Libro> listarLivros() {
+        return libroRepositorio.findAll();
+    }
+
+    @Override
+    public Libro buscarLibroPorId(Integer idLibro) {
+        Libro libro = libroRepositorio.FindById(idLibro).orElse(null);
+        return libro;
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+        libroRepositorio.save(libro);
+    }
+
+    @Override
+    public void elminarLibro(Libro libro) {
+        libroRepositorio.delete(libro);
+    }
 }
