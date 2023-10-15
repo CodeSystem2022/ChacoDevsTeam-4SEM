cb79267c1b139675d59cecc8d09453895bbdf852
Federico65744 <113891588+Federico65744@users.noreply.github.com>
2023-10-14 22:07:49 -0300

    cambio para algoritmo mas complejo ticket #243 (#255)

--- Node/02-Fundamentos_node/async/callback.js
+++ Node/02-Fundamentos_node/async/callback.js
@@ -1,11 +1,20 @@
-function soyAsincrona(miCallback){
+function hola(nombre, miCallback){
     setTimeout( function (){
-        console.log('Hola,soy una funcion asincrona');
+        console.log('Hola '+nombre);
         miCallback();
     },1000);
 }
 
+function adios(nombre, otroCallback) {
+    setTimeout(function (){
+        console.log('Adios', nombre);
+        otroCallback()
+    }, 1000);
+}
+
 console.log('Iniciando el proceso...')
-soyAsincrona(function(){
-    console.log('Terminando el proceso...')
+hola('Carlos', function(){
+    adios('Carlos', function() {
+        console.log('Termino el proceso...');
+    });
 });
