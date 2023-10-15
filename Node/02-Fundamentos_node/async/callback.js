4a7a98819d76c49270db0fc82ec607ab7f30645c
Federico65744 <113891588+Federico65744@users.noreply.github.com>
2023-10-14 21:00:02 -0300

    se genero callback ticket #243 (#254)

--- Node/02-Fundamentos_node/async/callback.js
+++ Node/02-Fundamentos_node/async/callback.js
@@ -1,6 +1,7 @@
-function soyAsincrona(){
-    setTimeout( function (miCallback){
+function soyAsincrona(miCallback){
+    setTimeout( function (){
         console.log('Hola,soy una funcion asincrona');
+        miCallback();
     },1000);
 }
 
