b0a6cd52838c874a282ccb92866ca47bbf043c2f
Agustin Gutierrez <yo_agustingutierrez@hotmail.com>
2023-10-15 00:08:08 -0300

    NODE - conceptos, variables compartidas y error-#245

--- Node/02-Fundamentos_node/async/callback.js
+++ Node/02-Fundamentos_node/async/callback.js
@@ -1,20 +1,23 @@
 function hola(nombre, miCallback){
     setTimeout( function (){
-        console.log('Hola '+nombre);
-        miCallback();
+        console.log('Hola '+ nombre);
+        miCallback(nombre);
     },1000);
 }
 
 function adios(nombre, otroCallback) {
     setTimeout(function (){
-        console.log('Adios', nombre);
+        console.log('Adios '+ nombre);
         otroCallback()
     }, 1000);
 }
 
 console.log('Iniciando el proceso...')
-hola('Carlos', function(){
-    adios('Carlos', function() {
+hola('Carlos', function(nombre) {
+    adios(nombre, function() {
         console.log('Termino el proceso...');
     });
 });
+
+//hola('Carlos', function(){});
+//adios('Carlos', function(){});
