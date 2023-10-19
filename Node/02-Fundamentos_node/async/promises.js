7d05e7452b542eca792ba55fe50481ae958991c0
NicoBalbuena <112787278+NicoBalbuena@users.noreply.github.com>
2023-10-19 11:22:17 -0300

    promises.js (#279)
    Se crea nuevo archivo

---
+++ Node/02-Fundamentos_node/async/promises.js
@@ -0,0 +1,20 @@
+function hola(nombre, miCallback){
+    setTimeout( function (){
+        console.log('Hola '+nombre);
+        miCallback(nombre);
+    },1000);
+}
+
+function hablar(callbackHablar){
+    setTimeout( function () {
+        console.log('bla bla bla bla');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function (){
+        console.log('Adios '+ nombre);
+        otroCallback()
+    }, 1000);
+}
