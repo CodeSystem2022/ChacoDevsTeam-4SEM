7a0fa20fd3de7c1d8a98bbd19745c43a98439a03
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-10-20 00:27:28 -0300

    JAVA SPRINGBOOT-Proyecto Tienda de Libros con SpringBoot y Swing-Terminando con el botón Modificar (#281)
    Se realizó lo pedido en el ticket.

--- JAVA/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ JAVA/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -110,7 +110,17 @@ public class LibroFrom extends JFrame {
                 libroTexto.requestFocusInWindow();
                 return;
             }
-           
+           // Llenamos el objeto libro a actualizar
+            int idLibro = Integer.parseInt(idTexto.getText());
+            var nombreLibro = libroTexto.getText();
+            var autor = autorTexto.getText();
+            var precio = Double.parseDouble(precioTexto.getText());
+            var existencias = Integer.parseInt(existenciaTexto.getText());
+            var libro = new Libro(idLibro, nombreLibro, autor, precio, existencias);
+            libroServicio.guardarLibro(libro);
+            mostrarMensaje("Se modifico el libro...");
+            limpiarFormulario();
+            listarLibros();
         }
     }
 
