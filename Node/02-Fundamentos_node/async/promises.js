de2319df20bd259403ea71c57e3cd8440b285807
ludmilailen <ludmilaailenrss@gmail.com>
2023-10-20 11:54:00 -0300

    NODE-Promises - Resolve reject - Aplicando Promise en la función hola#275
    Creamos la fuction. Iniciamos y terminamos proceso.

--- Node/02-Fundamentos_node/async/promises.js
+++ Node/02-Fundamentos_node/async/promises.js
@@ -1,8 +1,11 @@
-function hola(nombre, miCallback){
-    setTimeout( function (){
-        console.log('Hola '+nombre);
-        miCallback(nombre);
-    },1000);
+function hola(nombre){
+    return new Promise(function (resolve, reject) {
+        setTimeout( function (){
+            console.log('Hola '+nombre);
+            resolve(nombre);
+        }, 1000);
+    });
+    
 }
 
 function hablar(callbackHablar){
@@ -13,8 +16,14 @@ function hablar(callbackHablar){
 }
 
 function adios(nombre, otroCallback) {
-    setTimeout(function (){
+    setTimeout( function() {
         console.log('Adios '+ nombre);
         otroCallback()
-    }, 1000);
+    }, 1500);
 }
+
+console.log('Iniciando el proceso...');
+hola('Ariel')
+    .then((nombre) => {
+        console.log('Terminando el proceso');
+    })
