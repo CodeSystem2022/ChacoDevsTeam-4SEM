d28deb005add2d4e26ff2a39e80ea9d1a59c978d
Juan Carlos Gutierrez <108031724+DiosJuanVero2015@users.noreply.github.com>
2023-10-22 18:57:29 -0300

    NODE-Promises - Resolve reject - Utilizamos reject con el .catch#278 (#288)

--- Node/02-Fundamentos_node/async/promises.js
+++ Node/02-Fundamentos_node/async/promises.js
@@ -1,16 +1,16 @@
-function hola(nombre){
+function hola(nombre) {
     return new Promise(function (resolve, reject) {
-        setTimeout( function (){
-            console.log('Hola '+nombre);
+        setTimeout(function () {
+            console.log('Hola ' + nombre);
             resolve(nombre);
         }, 1000);
     });
-    
+
 }
 
-function hablar(nombre){
+function hablar(nombre) {
     return new Promise((resolve, reject) => { //usamos la sintaxis ES6
-        setTimeout( function () {
+        setTimeout(function () {
             console.log('bla bla bla bla');
             resolve(nombre);
         }, 1000);
@@ -18,16 +18,17 @@ function hablar(nombre){
 }
 
 function adios(nombre) {
-    return new Promise( (resolve, reject) => {
-        setTimeout( function() {
-            console.log('Adios '+ nombre);
-            resolve()
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Adios ' + nombre);
+            //resolve();
+            reject('Hay un error');
         }, 1000);
-    })
+    });
 }
 
-//Llamamos a la funcion 
-console.log('Iniciando el proceso...');
+//Llamamos a la funcion
+console.log('Iniciando el proceso... ');
 hola('Ariel')
     .then(hablar)
     .then(hablar)
@@ -35,4 +36,8 @@ hola('Ariel')
     .then(adios)
     .then((nombre) => {
         console.log('Terminando el proceso');
+    })
+    .catch(error => {
+        console.log('Ha habido un error: ');
+        console.log(error);
     })
