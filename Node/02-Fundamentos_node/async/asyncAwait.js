34a04729af13b3334c5a949bbacea96bb13bbd26
Juan Carlos Gutierrez <DiosJuanVero2015@gmail.com>
2023-10-30 20:49:07 -0300

    NODE-Definimos los resultados con async/await#301

--- Node/02-Fundamentos_node/async/asyncAwait.js
+++ Node/02-Fundamentos_node/async/asyncAwait.js
@@ -27,30 +27,51 @@ async function adios(nombre) {
     });
 }
 
-//await hola('Ariel'); //Esto es una mala sintaxis
-//await solo es valido dentro de una funcion asincrona
-async function main(){
-	let nombre = await hola('Ariel');
-	await hablar();
-	await hablar();
-	await hablar();
-	await adios(nombre);	
-	console.log('Termina el proceso...')
-}
+
+
+
+
+
+
+
+
+
 
 //console.log('Empezamos en proceso...')
 //main();
 //console.log('Esta va ser la segunda instruccion')
 
+//Codigo en ingles
+//es asincrono y retorna una promesa
+
+function sayHello(name){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Hello " + name);
+            resolve(name);
+        }, 1000);
+    })
+}
+
+function sayBye(name){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Goodbye " + name);
+            resolve(name);
+        }, 1000);
+    })
+}
 
 async function conversation(name){
-	console.log("Code in english");
-	console.log("Starting async process...";
-	await sayHello(name);
-	await talk();
-	await talk();
-	await sayBye(name);	
-	console.log('Process completed')
+    console.log("Code in english");
+    console.log("Starting async process...");
+
+    await sayHello(name);
+    await talk();
+    await talk();
+    await sayBye(name);
+
+    console.log('Process completed');
 }
 
 conversation("Ariel");
