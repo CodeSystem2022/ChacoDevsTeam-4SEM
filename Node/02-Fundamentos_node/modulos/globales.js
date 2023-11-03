aa4f29bff35a03cebb58805884bba41e758a8514
Federico65744 <cvaldez250@outlook.com>
2023-11-02 18:40:22 -0600

    se agregó la carpeta modulos y el archivo globales.js(ticket#320)

---
+++ Node/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,16 @@
+// this === global = true
+
+//Mostrar algo en consola
+console.log();
+
+//Mostrar un mensaje en forma de error
+console.error();
+
+//Ejecutar un código después de un intervalo de tiempo
+setTimeout(() => {});
+
+//Ejecutar un código cada intervalo de tiempo
+setInterval(() => {});
+
+//Da prioridad de ejecución a una función asincrona
+setImmediate(() => {});
