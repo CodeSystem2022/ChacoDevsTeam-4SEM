d739f2ecedb82ccd391006d36238bf6ec9090cdf
Tatiana Tamara, Giménez Ríos <103543709+TatianaGimenezRios@users.noreply.github.com>
2023-11-10 09:09:14 -0300

    NODE-Módulos -> File System-Introducción#331 (#340)

--- Node/02-Fundamentos_node/modulos/globales.js
+++ Node/02-Fundamentos_node/modulos/globales.js
@@ -19,7 +19,7 @@
 
 let i = 0;
 let intervalo = setInterval(() => {
-   console.log("Hola");
+console.log("Hola");
     if (i === 3) {
         clearInterval(intervalo); //detenemos la funcion
     }
