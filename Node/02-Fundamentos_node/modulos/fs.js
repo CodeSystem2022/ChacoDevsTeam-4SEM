e22cc81284d7d96ddf2ab40743fdad02635cdf40
NicoBalbuena <112787278+NicoBalbuena@users.noreply.github.com>
2023-11-10 13:43:39 -0300

    Ticket Node (#342)
    Se completa

--- Node/02-Fundamentos_node/modulos/fs.js
+++ Node/02-Fundamentos_node/modulos/fs.js
@@ -1,2 +1,14 @@
 const fs = require('fs');
 
+//Primero leemos el archivo.txt
+function leer(ruta,cb){
+     fs.readFile(ruta,(err,data)=>
+    {
+        cb(data.toString())
+    })
+}
+   
+
+leer(`${__dirname}/archivo.txt`,console.log)
+
+
