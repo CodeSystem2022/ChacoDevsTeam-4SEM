1dc7f1d795739d811749070a198eeb8eb3a42fc9
Juan Carlos Gutierrez <108031724+DiosJuanVero2015@users.noreply.github.com>
2023-11-10 21:26:18 -0300

    PYTHON - Entorno Profesional -> Parte 4 - mejoras y ajustes 1#338 (#346)

--- PYTHON/py-project/app/charts.py
+++ PYTHON/py-project/app/charts.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(name, labels, values):
-  fig, ax = plt.subplots()
-  ax.bar(labels, values)
-  plt.savefig('pie.png')
-  plt.close()
+def generate_bar_chart(name, Labels, values):
+	fig, ax = plt.subplots()
+	ax.bar(labels, values)
+	plt.savefig(f'./imgs/{name}.png')
+	plt.close()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
